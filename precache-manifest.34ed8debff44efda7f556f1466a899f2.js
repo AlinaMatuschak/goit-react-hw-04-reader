820ae7fd02d1c36e50843140b8c3e71fbe99ad57
Alina <alina90565@gmail.com>
2019-11-16 20:17:25 +0200

    Updates

---
+++ precache-manifest.34ed8debff44efda7f556f1466a899f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b65b9f7d93773651f482d099093e253",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "4d3723e45cd146102055",
+    "url": "/goit-react-hw-04-reader/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a9dd2160665eb82427ae",
+    "url": "/goit-react-hw-04-reader/static/css/main.0d3a23c2.chunk.css"
+  },
+  {
+    "revision": "4d3723e45cd146102055",
+    "url": "/goit-react-hw-04-reader/static/js/2.4fa664b4.chunk.js"
+  },
+  {
+    "revision": "a9dd2160665eb82427ae",
+    "url": "/goit-react-hw-04-reader/static/js/main.9095f9ee.chunk.js"
+  },
+  {
+    "revision": "c1ff3a6282e77220e0b8",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.9ed90bf6.js"
+  }
+]);
